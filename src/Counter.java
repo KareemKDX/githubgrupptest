887d45b8a0fc01f31c777b0a7575f61b734c3563
Kareem D <drammeh@msn.com>
2023-10-26 12:54:11 +0200

    test

--- src/Counter.java
+++ src/Counter.java
@@ -5,7 +5,7 @@ public class Counter {
         int i = 6;
         int x = 3;
         int total = i + x;
-        System.out.println(total + "testefel");
+        System.out.println(total + "testefel bdwjbhc");
 
 
     }
